136199503d9d98c3776397d06bf1d0761304e770
EagleCharles <sirnwa21@gmail.com>
2023-10-05 16:55:54 +0100

    test 2

---
+++ slack_notification.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Slack webhook URL
+SLACK_WEBHOOK_URL="$1"
+
+# JSON payload for Slack message
+PAYLOAD='{"text": ":fire: Lambda build started! :rocket:"}'
+
+# Send the Slack notification
+curl -X POST --data-urlencode "payload=$PAYLOAD" "$SLACK_WEBHOOK_URL"
